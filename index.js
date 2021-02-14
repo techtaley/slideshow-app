f759da3f505fe2d159a3b2d081da51f888d3e8d1
TaLoveous Goodwin <TaLoveous@expansivedesigns.com>
2021-02-13 19:08:06 -0500

    updated index.js

--- index.js
+++ index.js
@@ -17,35 +17,3 @@ app.listen(port, () => {
     console.log('Flower app is listening on http://localhost:${port}')
 })
 
-
-
-
-// const flowers = require("./flowers_api.json")
-// const port = process.env.PORT || 3000
-
-// //routes
-// app.get("/", (req, res) => {
-//         rest.send("Welcome to my image banner")
-// })
-
-// app.get("/flowers", (req, res) => {
-//         res.send(flowers)
-// })
-
-// app.listen(port, () => {
-//         console.log(`Flower app is listening on port http://localhost:${port}`)
-// })
-
-
-// async function start(){
-//         const res = await fetch("http://dog.ceo/api/breeds/list/all")
-//         const data = await res.json()
-//         getFlowers(data.message)  
-// }
-// start()
-
-// function getDogs(){
-//         document.getElementById("flowers").innerHTML = `
-//         <div></div>
-//         `
-// }
